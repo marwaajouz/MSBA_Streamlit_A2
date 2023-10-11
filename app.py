e37bd537f391fdd4e34e8ef23039452162b2171b
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-10-11 08:12:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,48 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+st.subheader('Number of Cigarettes Consumed / Smoking Person / Day')
+# Create a Streamlit slider to select the year
+selected_year = st.slider('Select Year', 2000, 2012)
+
+# Filter data for the selected year
+filtered_data = data[data['Year'] == selected_year]
+
+# Create a bar chart using Matplotlib
+fig, ax = plt.subplots()
+ax.bar(filtered_data["Country"], filtered_data["Data.Daily cigarettes"])
+ax.set_xlabel("Country")
+ax.set_ylabel("Cigarettes per Day")
+ax.set_title(f"Number of Cigarettes Consumed / Smoking Person / Day / Country ({selected_year})")
+
+plt.xticks(rotation=90)
+# Display the chart in Streamlit
+st.pyplot(fig)
+
+
+st.subheader('Percentage of Female vs Male Smokers and their change across the years')
+st.write('Select a country of your interest')
+selected_country = st.selectbox('Select a country', data['Country'].unique())
+
+# Radio button for selecting 'percentage' or 'numbers'
+data_type = st.radio('Select data type', ['percentage', 'numbers'])
+filtered_data = data[data['Country'] == selected_country]
+
+colors = ['blue', 'red']
+
+plt.figure(figsize=(10, 6))
+
+if data_type == 'percentage':
+    plt.plot(filtered_data['Year'], filtered_data['Data.Percentage.Male'], label='Male Percentage', color=colors[0])
+    plt.plot(filtered_data['Year'], filtered_data['Data.Percentage.Female'], label='Female Percentage', color=colors[1])
+    plt.ylabel('Percentage')
+    plt.title(f'{selected_country} Percentage Data')
+else:
+    plt.plot(filtered_data['Year'], filtered_data['Data.Smokers.Male'], label='Male Smokers', color=colors[0])
+    plt.plot(filtered_data['Year'], filtered_data['Data.Smokers.Female'], label='Female Smokers', color=colors[1])
+    plt.ylabel('Number of Smokers')
+    plt.title(f'{selected_country} Number of Smokers')
+
+plt.xlabel('Year')
+plt.legend()
+st.pyplot(plt)
